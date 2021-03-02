eb3bb56a5f6afd5909a2c95d7be8d28b18a898b6
Radhika Joshi <radhika7002@gmail.com>
2021-03-02 07:20:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+//exercise1
+const obj={myAge:25,mothersAge:48};
+const objnew={siblingAge:obj.myAge-6,...obj};
+console.log("initial object:"+ "\n myAge: "+ obj.myAge + "\n mother's Age: "+ obj.mothersAge);
+console.log("--------------------------------------------------------");
+console.log("New object with sibling's age apeended:"+"\n sibling's age: "+objnew.siblingAge +"\n myAge: "+ objnew.myAge + "\nmother's Age: "+ objnew.mothersAge );
+console.log("_________________________________________________________________");
+
+//exercise2
+
